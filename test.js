7e2a711da448682052d806bde7f9f4d9813ca3ac
Kennedy Rose <kennedy@kennedyrose.com>
2017-08-17 10:09:23 -0500

    Crawl reviews

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict'
 const questionsCrawler = require('./app.js')
 // https://www.amazon.com/ask/questions/asin/B003B809IG/1/ref=ask_ql_psf_ql_hza?sort=SUBMIT_DATE
-questionsCrawler('B003B809IG')
+questionsCrawler('B0076S64T0')
 	.then(console.log)
 	.catch(console.error)
